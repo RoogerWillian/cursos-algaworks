b1bc4a97bba5e629c2042d6bd402efac40b6d91f
= <roger.nizoli@gmail.com>
2016-06-07 20:08:19 -0300

    commit final

---
+++ workshop-web-design-responsivo/mypilas/js/app.js
@@ -0,0 +1,6 @@
+var botaoChaveador = document.querySelector('.js-botao-chaveador');
+
+botaoChaveador.onclick = function (){
+	var menu = document.querySelector('.js-menu');
+   	menu.classList.toggle('menu--exibindo');
+}
